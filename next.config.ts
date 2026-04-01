162982c7817469fa3f4b5178d6a4a610779c7c5f
BILLALE H. <billaleh.@mac.home>
2026-04-01 19:49:24 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // Ces lignes sont nécessaires pour le déploiement sur GitHub Pages si votre site est dans un sous-dossier
-  // Remplacez '/alhambra-web' par le nom de votre dépôt GitHub si ce n'est pas 'alhambra-web'
+  // Correct configuration for GitHub Pages at xeloriom.github.io/alhambra-web/
   basePath: '/alhambra-web',
-  assetPrefix: '/alhambra-web',
+  assetPrefix: '/alhambra-web/',
 };
 
 export default nextConfig;
