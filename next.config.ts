a5a3bd7a3fcab90eeadc31a5dfcc4180ad8c5656
BILLALE H. <billaleh.@mac.home>
2026-04-01 19:43:19 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Si vous déployez sur xeloriom.github.io/ALHAMBRA-WEB, décommentez les lignes ci-dessous :
+  // basePath: '/alhambra-web',
+  // assetPrefix: '/alhambra-web',
 };
 
 export default nextConfig;
